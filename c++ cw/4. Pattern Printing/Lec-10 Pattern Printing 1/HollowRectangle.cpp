b962b8bb9a7b7faa7d26d8417ea43f772c0c8a32
Saksham Jain <sakshamjain735@gmail.com>
2026-08-01 22:49:41 +0530

    Lec-10 Pattern Printing 1

---
+++ c++ cw/4. Pattern Printing/Lec-10 Pattern Printing 1/HollowRectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows,cols;
+    cout<<"Enter Number Of Rows : ";
+    cin>>rows;
+    cout<<"Enter Number Of Columns : ";
+    cin>>cols;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=cols;j++){
+            if(i==1 || i==rows || j==1 || j==cols) cout<<"* ";
+            else cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
---
+++ c++ cw/4. Pattern Printing/Lec-10 Pattern Printing 1/StarRectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows,cols;
+    cout<<"Enter Number Of Rows : ";
+    cin>>rows;
+    cout<<"Enter Number Of Columns : ";
+    cin>>cols;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=cols;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
