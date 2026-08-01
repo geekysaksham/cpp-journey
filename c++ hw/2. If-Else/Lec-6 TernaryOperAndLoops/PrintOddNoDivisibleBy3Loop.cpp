f918d75c54abbf500d379642179566513aab5acc
Saksham Jain <sakshamjain735@gmail.com>
2026-08-02 00:05:40 +0530

    Lec-6 TernaryOperAndLoops

---
+++ c++ hw/2. If-Else/Lec-6 TernaryOperAndLoops/PrintOddNoDivisibleBy3Loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for(int i=1;i<=100;i++){
+    //     if(i%2!=0 && i%3==0) cout<<i<<" ";
+    // }
+    
+    // for(int i=1;i<=100;i+=2){
+    //     if(i%3==0) cout<<i<<" ";
+    // }
+
+    for(int i=3;i<=99;i+=6){
+        cout<<i<<" ";
+    }
+}
