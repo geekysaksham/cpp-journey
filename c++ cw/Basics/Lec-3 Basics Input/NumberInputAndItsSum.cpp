deee4a911d70267a813f61bf6216c10df567585f
Saksham Jain <sakshamjain735@gmail.com>
2026-08-01 22:05:53 +0530

    Lec-3 Basics Input

---
+++ c++ cw/Basics/Lec-3 Basics Input/NumberInputAndItsSum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"Enter All Numbers : ";
+    cin>>n1>>n2;
+    int sum=n1+n2;
+    cout<<"Sum Of Numbers = "<<sum;
+}
