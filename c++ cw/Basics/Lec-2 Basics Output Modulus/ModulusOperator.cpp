a1d27da6b9d92139951e92f26bd6d140df31bfc9
Saksham Jain <sakshamjain735@gmail.com>
2026-08-01 21:59:06 +0530

    Lec-2 Basics Output Modulus

---
+++ c++ cw/Basics/Lec-2 Basics Output Modulus/ModulusOperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<5%2<<endl;
+    cout<<51%22<<endl;
+    cout<<25%5<<endl;
+    //cout<<25%0 => ERROR
+    //cout<<25/0 => ERROR
+    cout<<0%25<<endl;
+    cout<<0/25<<endl;
+    cout<<3%-4<<endl;
+    cout<<23%-7<<endl;
+    cout<<-7%12<<endl;
+    cout<<-24%7<<endl;
+}
